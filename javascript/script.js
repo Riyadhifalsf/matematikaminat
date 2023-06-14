0bc985bde6c38c7dfca93114913fee22628564f2
Riyadhifalsf <gbzleopard@gmail.com>
2023-06-14 11:05:08 -0700

    menambahkan fitur form spreadsheet

--- javascript/script.js
+++ javascript/script.js
@@ -38,3 +38,40 @@ function showConfirmation() {
 	  window.location.href = "gamepuzzle/index.html";
 	}
   }
+
+//   Modal
+// Menampilkan modal saat tombol diklik
+document.getElementById('openModal').addEventListener('click', function() {
+	document.getElementById('myModal').style.display = 'block';
+  });
+  
+  // Menutup modal saat ikon close diklik
+  document.getElementById('closeModal').addEventListener('click', function() {
+	document.getElementById('myModal').style.display = 'none';
+  });
+  
+  // Menutup modal saat di luar area modal diklik
+  window.addEventListener('click', function(event) {
+	var modal = document.getElementById('myModal');
+	if (event.target === modal) {
+	  modal.style.display = 'none';
+	}
+  });
+  
+  // Mengirim data ulasan saat formulir dikirim
+  document.getElementById('reviewForm').addEventListener('submit', function(event) {
+	event.preventDefault();
+  
+	// Ambil nilai dari isian
+	var name = document.getElementById('name').value;
+	var message = document.getElementById('message').value;
+	var rating = document.querySelector('input[name="rating"]:checked').value;
+  
+	// Lakukan apa yang Anda inginkan dengan data ulasan
+	// Contoh: Kirim ke server menggunakan AJAX, simpan di database, dll.
+  
+	// Setelah data ulasan dikirim, tutup modal
+	document.getElementById('myModal').style.display = 'none';
+  });
+  
+  
